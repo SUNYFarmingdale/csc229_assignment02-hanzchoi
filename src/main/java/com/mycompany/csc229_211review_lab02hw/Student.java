281115993ed1c7b60a910846745a01a79f0ecad1
swami <swami@TABLET-4SO2JAOT>
2023-02-05 17:52:25 -0500

    created get and set for address and GPA

--- src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -2,15 +2,37 @@ package com.mycompany.csc229_211review_lab02hw;
 
 /**
  *
- * @author MoaathAlrajab
+ * @author Hanbi Hanz Choi
  */
-public class Student {
-	// ToDo 1: Make this class a child of Person
+
+// ToDo 1: Make this class a child of Person
+// By using extends Person the Student bcame the child class of Person
+public class Student extends Person{
+    
+    private String address;
+    private double GPA;
 	
-	// ToDo 2: Fix the resulting errors
+    // ToDo 2: Fix the resulting errors
+    // They wanted to override the get and set address 
+    // I am not sure exacly why this is the case
+    // Maybe something to do with the fact that those two methods are abstract
+    @Override 
+    public String getAddress(){
+        return this.address;
+    }
+    
+    @Override
+    public void setAddress(String address){
+        this.address = address;
+    }
 	
-	// ToDo 3: Add a field for GPA and create setter and getter
-	
-	// ToDo 4: Add comments to your code
+    // ToDo 3: Add a field for GPA and create setter and getter
+    public double getGPA(){
+        return this.GPA;
+    }
+    
+    public void setGPA(double GPA){
+        this.GPA = GPA;
+    }
 
 }
